b85a51eb23e907e949bda44cd8574e30920c3354
AleksGri <aleks032118@gmail.com>
2021-10-05 17:15:56 -0700

    FEAT(script.js) add calculator

--- scripts/script.js
+++ scripts/script.js
@@ -103,7 +103,7 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  countTimer('13 september 2021');
+  countTimer('15:20 5 october 2021');
 
   //Menu
   function toggleMenu(){
@@ -383,8 +383,39 @@ window.addEventListener('DOMContentLoaded', function() {
   slider();
 
   //Calculator
-  const calc = () => {
-    const calcBlock = document.querySelector('.calc-block');
+  const calc = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+          calcTypeValue = calcBlock.querySelector('.calc-type'),
+          calcSquareValue = calcBlock.querySelector('.calc-square'),
+          calcCountValue = calcBlock.querySelector('.calc-count'),
+          calcDayValue = calcBlock.querySelector('.calc-day'),
+          totalValue = calcBlock.querySelector('#total');
+
+    const countSum = () => {
+      let total = 0;
+      const calcSquare = calcSquareValue.value,
+            calcCount = calcCountValue.value,
+            calcDay = calcDayValue.value,
+            calcType = calcTypeValue.options[calcTypeValue.selectedIndex].value;
+
+      console.log(calcType);
+
+      if (calcType && calcSquare ) {
+        total = calcType*calcSquare*price;
+      }
+
+      if (calcCount > 1 ) {
+        total *= 1 + (calcCount - 1)/10;
+      }
+
+      if (calcDay && calcDay < 5 ) {
+        total *= 2;
+      } else if (calcDay && calcDay < 10 ) {
+        total *= 1.5;
+      }
+
+      totalValue.textContent = total;
+    };
 
     calcBlock.addEventListener('input', (event)=>{
       let target = event.target;
@@ -393,9 +424,16 @@ window.addEventListener('DOMContentLoaded', function() {
         target.value = target.value.replace(/\D/g, '');
       }
     });
+
+    calcBlock.addEventListener('change', (event)=>{
+      let target = event.target;
+      if(target.tagName === 'INPUT' || target.tagName === 'SELECT') {
+        countSum();
+      }
+    });
   };
 
-  calc();
+  calc(100);
 
 
   //Our team
